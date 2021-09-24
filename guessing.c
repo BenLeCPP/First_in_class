b2c74b04250d4c750592348200b40d78f9e4300a
Ben Le <hale@cpp.edu>
2021-09-24 16:54:49 -0700

    added save max number

--- guessing.c
+++ guessing.c
@@ -8,96 +8,136 @@ OPtion 3: user can choose to quit the game.
 #include <stdlib.h>
 #include <time.h>
 
-int main (void)
-{
-    int trueNumber, userGuess, attempt = 0;
-    int max =10;
-
-    trueNumber = rand() % max + 1;
+void Menu()
+{ 
     printf("Welcome to the Guessing Number Game!\n");
     printf("Press 1 to play a game\n");
     printf("Press 2 to change the max number\n");
     printf("Press 3 to quit\n");
     printf("");
 
-    int choice;
-    scanf("%d", &choice);
-    printf("You chose %d\n", choice);
+}
+void Game()
+{
+   Menu();
+   int trueNumber, userGuess, attempt = 0;
+   time_t t;
+   srand((unsigned) time(&t));
+   int max =10;
+   trueNumber = rand() % max + 1;
+   int choice;
+   scanf("%d", &choice);
+   printf("You chose %d\n", choice);
+   
+   switch(choice)
+   {
+   case 1:
+        printf("Let's start the game!\n");
+        printf("Guess a number between " "1 and %d\n", max);
 
-    switch(choice)
-    {
-        case 1:
-            printf("Let's start the game!\n");
-            printf("Guess a number between " "1 and %d\n", max);
+        do 
+        {
+            attempt++;
+            scanf("%d", &userGuess);
+            if (userGuess > trueNumber)
+            {
+                printf("Please guess a lower number\n");
+            }
+            else if (userGuess < trueNumber)
+            {
+                printf("Please guess a higher number\n");   
+            }
+            else if (userGuess == trueNumber)
+            {
+                printf("You guessed the correct number in %d" " times\n", attempt);
+                printf("");
+                Game(); 
+            }
+            else
+            {
+                printf("You entered an invalid number!\n");
+                printf("Game lost!");
+            }
 
+
+        } while (userGuess != trueNumber);
+        break;
+    
+    case 2:
+        printf("With this option, you may select another max number that is under 100\n");
+        printf("Please enter the max number you want\n");
+        scanf("%d", &max);
+        printf("Your new max number is %d\n", max);
+        again:
+        printf("Guess a number between " "1 and %d\n", max);
+        
+        if(0< max && max <100 & max >= trueNumber)
+        {
             do 
             {
-                attempt++;
                 scanf("%d", &userGuess);
                 if (userGuess > trueNumber)
-                {
-                    printf("Please guess a lower number\n");
-                }
+                    {
+                        printf("Please guess a lower number\n");
+                        attempt++;
+                    }
                 else if (userGuess < trueNumber)
-                {
-                    printf("Please guess a higher number\n");
-                    
-                }
+                    {
+                        printf("Please guess a higher number\n");
+                        attempt++;
+                    }
                 else if (userGuess == trueNumber)
-                {
-                    printf("You guessed the correct number in %d" " times\n", attempt);
-                    
-                }
+                    {
+                        printf("You guessed the correct number in %d" " times\n", attempt);
+                        attempt++;
+                        printf("");
+                        switch(choice)
+                        {
+                        case 1:
+                            scanf("%d", &choice);
+                            printf("You chose %d\n", choice);
+                            goto again;
+                            break;
+                        
+                        case 2 :
+                            Game();
+                            break;
+                        
+                        case 3:
+                           printf("You chose to quit the game!\n");
+                           printf("Thank you for playing and see you again!\n");
+                           break; 
+                        
+                        }
+                        
+                    }
                 else
-                {
-                    printf("You entered an invalid number!\n");
-                    printf("Game lost!");
-                }
+                    {
+                        printf("You entered an invalid number!\n");
+                        printf("Game lost!");
+                    }
 
 
             } while (userGuess != trueNumber);
-            break;
-
-        case 2:
-            printf("with this option, you may select another max number\n");
-            printf("Please enter the max number you want\n");
-            scanf("%d", &max);
-            printf("Your new max number is %d\n", max);
-            printf("Guess a number between " "1 and %d\n", max);
-
-            do 
-            {
-                scanf("%d", &userGuess);
-                if (userGuess > trueNumber)
-                {
-                    printf("Please guess a lower number\n");
-                    attempt++;
-                }
-                else if (userGuess < trueNumber)
-                {
-                    printf("Please guess a higher number\n");
-                    attempt++;
-                }
-                else if (userGuess == trueNumber)
-                {
-                    printf("You guessed the correct number in %d" " times\n", attempt);
-                    attempt++;
-                }
-                else
-                {
-                    printf("You entered an invalid number!\n");
-                    printf("Game lost!");
-                }
-
-
-            } while (userGuess != trueNumber);
-            break;
-
-        case 3: 
-        printf("You chose to quit the game!\n");
-        printf("Thank you for playing and see you again!");
+        }
+        else{
+            printf("You entered an invalid number!\n");
+            printf("Game lost!");
+        }
         break;
 
-    }
-    return 0;
+    case 3: 
+        printf("You chose to quit the game!\n");
+        printf("Thank you for playing and see you again!\n");
+        break;
+ 
+   }
+        
+}
+
+
+int main()
+{
+   Game();
+   return 0;
 }
