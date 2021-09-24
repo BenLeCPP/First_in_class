dc1af3f8d7b7fe6f0e904b38c3da3f3f64978522
Ben Le <hale@cpp.edu>
2021-09-24 14:17:51 -0700

    changed condition in if statement

--- restaurantbill.c
+++ restaurantbill.c
@@ -15,24 +15,26 @@ int main()
     float soup = 4.55;
     float sandwich = 13.25;
     float pizza = 22.35;
-    int i, n;
     time_t t;
+    int i = rand() % 5 + 1;
     int tax = 10;
     int tip = 10;
    
 
     srand((unsigned) time(&t));
   
-    if (rand () ==1 )
+    if (rand() % 4 == 1 )
     {
         float total = salad*0.1*0.1 + salad;
         printf("You ordered salad\n");
         printf("The meal costs you $%f\n", salad);
         printf("The tax amount is %d", tax);
+        printf( "%%\n");
         printf("In our restaurant, the tip amount is also added in the bill: %d", tip);
+        printf( "%%\n");
         printf("So your total bill is $%f\n", total);
     }
-    if (rand() ==2 )
+    else if (rand() % 4 == 2 )
     {
         float total = soup*0.1*0.1 + soup;
         printf("Your ordered soup\n");
@@ -44,7 +46,7 @@ int main()
         printf("The total bill is $%f\n", total);
 
     }
-    if (rand()==3 )
+    else if (rand() % 4 == 3 )
     {
         float total = sandwich*0.1*0.1 + sandwich;
         printf("You ordered sandwich\n");
@@ -55,7 +57,8 @@ int main()
         printf( "%%\n");
         printf("The total bill is $%f\n", total);
     }
-    else
+    else 
+
     {
         float total = pizza*0.1*0.1 + pizza;
         printf("You ordered pizza\n");
